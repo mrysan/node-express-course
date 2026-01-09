a34a7dc0ad61f71b9b308ef7fe02f9f945bef2fe
mrysan <mrysann@gmail.com>
2026-01-08 18:14:32 -0800

    week4 complete

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+const { products, people } = require("./data");
+const peopleRouter = require("./routes/people");
 
-app.listen(3000, (req, res) => {
-  console.log("Server Started on port 3000");
-});
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const date = new Date();
+  // month/day/year time
+  const time = `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()} `;
+  console.log(method, url, time);
+  next();
+};
 
+app.use(logger);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("./public"));
+
+app.use("/api/v1/people", peopleRouter);
+// app.get("/api/v1/people", (req, res) => {
+//   res.json(people);
+// });
+
+// app.post("/api/v1/people", (req, res) => {
+//   const name = req.body.name;
+//   if (!name) {
+//     res.status(400).json({ success: false, message: "Please provide a name" });
+//   } else {
+//     people.push({ id: people.length + 1, name: req.body.name });
+//     res.status(201).json({ success: true, name: req.body.name });
+//   }
+// });
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ message: "It worked!" });
@@ -59,4 +86,8 @@ app.get("/api/v1/query", (req, res) => {
 
 app.all("*", (req, res) => {
   res.status(404).send("Error 404: Page Not Found");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server Started on port 3000");
 });
