12a325e0752112f7328d8378b913a0f44edc48e5
mrysan <mrysann@gmail.com>
2026-01-15 15:12:10 -0800

    week5 complete

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,23 @@
+const Task = require("../models/Tasks");
+
+const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+};
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("update Tasks");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete Tasks");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
