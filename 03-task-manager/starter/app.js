0cc26daf3a24ee8df435d4d6260656ba9e2fc54f
mrysan <mrysann@gmail.com>
2026-01-22 16:26:27 -0800

    add async wrapper

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,6 +4,7 @@ const tasks = require("./routes/tasks");
 const port = 3000;
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
 //middleware
 app.use(express.static("./public"));
@@ -11,6 +12,7 @@ app.use(express.json());
 
 //routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 
 const start = async () => {
   try {
